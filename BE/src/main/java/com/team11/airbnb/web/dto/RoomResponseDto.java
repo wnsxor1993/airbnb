493cdabf9dca60ed54899ec2e81729dbde4f38cb
Meenzino <86910955+Minzino@users.noreply.github.com>
2022-06-02 18:53:44 +0900

    feature: Room 숙소찾기 DTO 생성

---
+++ BE/src/main/java/com/team11/airbnb/web/dto/RoomResponseDto.java
@@ -0,0 +1,11 @@
+package com.team11.airbnb.web.dto;
+
+public class RoomResponseDto {
+    private String name;
+    private int price;
+    private String thumbnailPath;
+    private double averageGrade;
+    private int numberOfReviews;
+    private boolean isWishList;
+
+}
