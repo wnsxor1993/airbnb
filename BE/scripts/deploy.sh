19c051f9de9265bef93edda5501d3c0ff39662c4
Meenzino <86910955+Minzino@users.noreply.github.com>
2022-06-04 20:25:14 +0900

    fix: 배포용 스크립트 파일 수정
    - deploy.sh에서 도커허브 레포지토리 변경

--- BE/scripts/deploy.sh
+++ BE/scripts/deploy.sh
@@ -1,14 +1,14 @@
 # 가동중인 jpa-shop 도커 중단 및 삭제
-sudo docker ps -a -q --filter "name=jpa-shop" | grep -q . && docker stop jpa-shop && docker rm jpa-shop | true
+sudo docker ps -a -q --filter "name=airbnb" | grep -q . && docker stop airbnb && docker rm airbnb | true
 
 # 기존 이미지 삭제
-sudo docker rmi meenzino/jpa-shop:1.0
+sudo docker rmi philsogood/airbnb:1.0
 
 # 도커허브 이미지 pull
-sudo docker pull meenzino/jpa-shop:1.0
+sudo docker pull philsogood/airbnb:1.0
 
 # 도커 run
-sudo docker run -d -p 80:8080 --name jpa-shop meenzino/jpa-shop:1.0
+sudo docker run -d -p 80:8080 --name airbnb philsogood/airbnb:1.0
 
 # 사용하지 않는 불필요한 이미지 삭제 -> 현재 컨테이너가 물고 있는 이미지는 삭제되지 않습니다.
 sudo docker rmi -f $(docker images -f "dangling=true" -q) || true
