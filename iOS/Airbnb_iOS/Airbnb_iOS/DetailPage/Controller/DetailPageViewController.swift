7fdfaa25380c81b57f5d674a58b03cb2467f84d6
wnsxor1993 <wnsxor1993@naver.com>
2022-06-06 16:14:03 +0900

    feat: ThumbnailImage, TitleText cell 구현

---
+++ iOS/Airbnb_iOS/Airbnb_iOS/DetailPage/Controller/DetailPageViewController.swift
@@ -0,0 +1,15 @@
+//
+//  DetailPageViewController.swift
+//  Airbnb_iOS
+//
+//  Created by juntaek.oh on 2022/06/06.
+//
+
+import UIKit
+
+class DetailPageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ iOS/Airbnb_iOS/Airbnb_iOS/DetailPage/View/ThumbnailImageCell.swift
@@ -0,0 +1,45 @@
+//
+//  ThumbnailView.swift
+//  Airbnb_iOS
+//
+//  Created by juntaek.oh on 2022/06/06.
+//
+
+import UIKit
+
+final class ThumbnailImageCell: UICollectionViewCell {
+    
+    static let identifier = "ThumbnailImageCell"
+
+    private let imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.setLayout()
+    }
+
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+
+    func configureImage(imageData: Data) {
+        imageView.image = UIImage(data: imageData)
+    }
+}
+
+private extension ThumbnailImageCell {
+    
+    func setLayout() {
+        NSLayoutConstraint.activate([
+            imageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            imageView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            imageView.topAnchor.constraint(equalTo: contentView.topAnchor),
+            imageView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor)
+        ])
+    }
+}
