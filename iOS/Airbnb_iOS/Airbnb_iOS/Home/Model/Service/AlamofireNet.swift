65149cc85dbb074edbecc71b743fef56e7ab0255
wnsxor1993 <wnsxor1993@naver.com>
2022-06-06 12:59:56 +0900

    feat: Alamofire 연결 및 GET 메서드 구현

---
+++ iOS/Airbnb_iOS/Airbnb_iOS/Home/Model/Service/AlamofireNet.swift
@@ -0,0 +1,31 @@
+//
+//  AlamofireNet.swift
+//  Airbnb_iOS
+//
+//  Created by juntaek.oh on 2022/06/06.
+//
+
+import Alamofire
+
+struct AlamofireNet {
+    
+    func getNetwork<T: Codable>(url: String, completion handler: @escaping (T?) -> Void) {
+        guard let validURL = URL(string: url) else { return }
+        
+        let validRequest = AF.request(validURL, method: .get).validate(statusCode: 200..<300)
+        
+        validRequest.response { response in
+            switch response.result {
+            case .success(let value):
+                guard let validValue = value else { return }
+                
+                let decodedValue: T? = JSONConverter.decodeJsonObject(data: validValue)
+                
+                handler(decodedValue)g
+                
+            case .failure(_):
+                return
+            }
+        }
+    }
+}
