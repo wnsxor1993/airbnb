7424ec79109014aa1b7f320d5e0c54fc7131b366
Sol <hskim6521@naver.com>
2022-06-06 12:10:56 +0900

    refactor: numberFormatter를 활용해 Int를 가격 문자열로 바꾸는 로직 간단하게 표현

--- iOS/Airbnb_iOS/Airbnb_iOS/Utility/Extension/Int+.swift
+++ iOS/Airbnb_iOS/Airbnb_iOS/Utility/Extension/Int+.swift
@@ -9,26 +9,9 @@ import Foundation
 
 extension Int {
     func toKRW() -> String {
-        var reversed = String("\(self)".reversed())
-        var splitted = [String]()
-        let offset = 3
-
-        if String(self).count <= offset {
-            return "\(self)원"
-        }
-
-        while reversed.count > offset {
-            let startPoint = reversed.startIndex
-            let endPoint = reversed.index(startPoint, offsetBy: offset)
-
-            splitted.append(String(reversed[startPoint..<endPoint].reversed()))
-            reversed.removeSubrange(startPoint..<endPoint)
-
-            if reversed.count <= offset {
-                splitted.append(String(reversed.reversed()))
-            }
-        }
-
-        return splitted.reversed().joined(separator: ",") + "원"
+        let numberFormatter = NumberFormatter()
+        numberFormatter.numberStyle = .decimal
+        let price = numberFormatter.string(from: self as NSNumber) ?? ""
+        return "\(price)원"
     }
 }
