059b7b7448cd49876b663342c4117954e5be2fbb
Meenzino <86910955+Minzino@users.noreply.github.com>
2022-06-06 16:17:07 +0900

    feat: MainEvent(히어로 이미지 영역) 엔티티 생성
    - IOS 기획서에 첫번째 페이지에서 히어로 이미지 영역의 내용 및 이미지 주소를 담당하는 클래스

---
+++ BE/src/main/java/com/team11/airbnb/web/dto/MainEventDto.java
@@ -0,0 +1,16 @@
+package com.team11.airbnb.web.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class MainEventDto {
+
+    private String title;
+    private String label;
+    private String imagePath;
+    private String buttonText;
+
+
+}
