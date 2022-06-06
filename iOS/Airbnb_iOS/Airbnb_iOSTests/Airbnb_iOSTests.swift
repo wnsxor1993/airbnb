910210c53164e388ee1f17003547038c63c94d3e
wnsxor1993 <wnsxor1993@naver.com>
2022-06-06 13:34:32 +0900

    feat: UnitTest 기능 추가

---
+++ iOS/Airbnb_iOS/Airbnb_iOSTests/Airbnb_iOSTests.swift
@@ -0,0 +1,31 @@
+//
+//  Airbnb_iOSTests.swift
+//  Airbnb_iOSTests
+//
+//  Created by juntaek.oh on 2022/06/06.
+//
+
+import XCTest
+
+class Airbnb_iOSTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        
+    }
+
+    override func tearDownWithError() throws {
+        
+    }
+
+    func testExample() throws {
+        
+    }
+
+    func testPerformanceExample() throws {
+        
+        measure {
+            
+        }
+    }
+
+}
