e000d4f9f2d82b6478331067b285ce8abb108fda
Sol <hskim6521@naver.com>
2022-06-08 00:36:50 +0900

    feat: AccomodationsViewController와 View 구현 (wnsxor1993/airbnb_team11#59)

---
+++ iOS/Airbnb_iOS/Airbnb_iOS/FindAccomodation/View/AccomodationsView.swift
@@ -0,0 +1,59 @@
+//
+//  AccomodationsView.swift
+//  Airbnb_iOS
+//
+//  Created by 김한솔 on 2022/06/07.
+//
+
+import UIKit
+
+final class AccomodationsView: UIView {
+
+    private lazy var accomodationsCollectionView: UICollectionView = {
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: getCollectionViewLayout())
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.isScrollEnabled = true
+        collectionView.showsHorizontalScrollIndicator = false
+        collectionView.showsVerticalScrollIndicator = false
+        collectionView.clipsToBounds = true
+
+        collectionView.register(AccomodationOptionCell.self, forCellWithReuseIdentifier: AccomodationOptionCell.identifier)
+        collectionView.register(CountAccomodationsCell.self, forCellWithReuseIdentifier: CountAccomodationsCell.identifier)
+        collectionView.register(AccomodationsCell.self, forCellWithReuseIdentifier: AccomodationsCell.identifier)
+        return collectionView
+    }()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = .white
+        addSubview(accomodationsCollectionView)
+        layoutMargins = UIEdgeInsets(top: 32, left: 16, bottom: 0, right: 16)
+    }
+
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+
+    func setDataSource(_ dataSource: AccomodationsCollectionDataSource) {
+        accomodationsCollectionView.dataSource = dataSource
+    }
+
+    override func layoutSubviews() {
+        NSLayoutConstraint.activate([
+            accomodationsCollectionView.topAnchor.constraint(equalTo: topAnchor),
+            accomodationsCollectionView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            accomodationsCollectionView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            accomodationsCollectionView.bottomAnchor.constraint(equalTo: bottomAnchor)
+        ])
+    }
+}
+
+private extension AccomodationsView {
+    func getCollectionViewLayout() -> UICollectionViewCompositionalLayout {
+
+        UICollectionViewCompositionalLayout { (section, _) -> NSCollectionLayoutSection? in
+            return AccomodationViewCollectionLayout(section: section).create()
+        }
+    }
+}
