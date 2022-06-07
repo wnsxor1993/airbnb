e276981e6711734ac4e561433156b475464fa07a
Sol <hskim6521@naver.com>
2022-06-07 11:36:57 +0900

    chore: 실제 형태로 mock.json 형식 변경 및 그로 인한 DTO 구조 변화 (wnsxor1993/airbnb_team11#54)

---
+++ iOS/Airbnb_iOS/Airbnb_iOS/Home/Model/HomeDto.swift
@@ -0,0 +1,32 @@
+//
+//  HomeDto.swift
+//  Airbnb_iOS
+//
+//  Created by 김한솔 on 2022/06/06.
+//
+
+import Foundation
+
+struct HomeDto: Codable {
+    let mainEventDto: MainEventDto
+    let aroundSpotDto: [AroundSpotDto]
+    let themeSpotDto: [ThemeSpotDto]
+}
+
+struct AroundSpotDto: Codable {
+    let title: String
+    let distance: Int
+    let imagePath: String
+}
+
+struct MainEventDto: Codable {
+    let title: String
+    let label: String
+    let imagePath: String
+    let buttonText: String
+}
+
+struct ThemeSpotDto: Codable {
+    let imagePath: String
+    let title: String
+}
--- iOS/Airbnb_iOS/Airbnb_iOSTests/JSONConverterTests.swift
+++ iOS/Airbnb_iOS/Airbnb_iOSTests/JSONConverterTests.swift
@@ -16,53 +16,23 @@ final class JSONConverterTests: XCTestCase {
         let jsonData = try? Data(contentsOf: path!)
         XCTAssertNotNil(jsonData)
         
-        let result: TestDTO? = JSONConverter.decodeJsonObject(data: jsonData!)
+        let result: HomeDto? = JSONConverter.decodeJsonObject(data: jsonData!)
         XCTAssertNotNil(result)
     }
     
     func test_encodeJsonObject호출시_JSON파일로_인코딩되는지() {
-        let targetParam = TestDTO(
-            heroImage: .init(imageURL: "url", title: "title", heroImageDescription: "description", buttonTitle: "buttontitle"),
-            aroundSpot: .init(imageURL: "url", title: "title", location: [100.1, 100.2], 거리: 300),
-            themeSpot: .init(imageURL: "url", title: "title"))
+        let targetParam = HomeDto(
+            mainEventDto: .init(title: "title",
+                                label: "label",
+                                imagePath: "imagePath",
+                                buttonText: "buttonText"),
+            aroundSpotDto: [.init(title: "title",
+                                  distance: 0,
+                                  imagePath: "imagePath")],
+            themeSpotDto: [.init(imagePath: "imagePath",
+                                 title: "title")])
         
         let result = JSONConverter.encodeJsonObject(param: targetParam)
         XCTAssertNotNil(result)
     }
-}
-
-private extension JSONConverterTests {
-    struct TestDTO: Codable {
-        let heroImage: HeroImage
-        let aroundSpot: AroundSpot
-        let themeSpot: ThemeSpot
-
-        enum CodingKeys: String, CodingKey {
-            case heroImage = "hero_image"
-            case aroundSpot, themeSpot
-        }
-    }
-    struct AroundSpot: Codable {
-        let imageURL: String
-        let title: String
-        let location: [Double]
-        let 거리: Int
-    }
-
-    struct HeroImage: Codable {
-        let imageURL: String
-        let title, heroImageDescription, buttonTitle: String
-
-        enum CodingKeys: String, CodingKey {
-            case imageURL, title
-            case heroImageDescription = "description"
-            case buttonTitle
-        }
-    }
-
-    struct ThemeSpot: Codable {
-        let imageURL: String
-        let title: String
-    }
-
 }
