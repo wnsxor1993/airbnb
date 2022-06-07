232a93338afada0e878c2f43b1edad0d708b8e9f
Sol <hskim6521@naver.com>
2022-06-07 14:25:07 +0900

    chore: HeroImageCell의 subview들의 Height Constraints 수정 (wnsxor1993/airbnb_team11#57)

--- iOS/Airbnb_iOS/Airbnb_iOS/Home/View/HeroImageCell.swift
+++ iOS/Airbnb_iOS/Airbnb_iOS/Home/View/HeroImageCell.swift
@@ -65,19 +65,16 @@ final class HeroImageCell: UICollectionViewCell {
 
             titleLabel.leadingAnchor.constraint(equalTo: imageView.layoutMarginsGuide.leadingAnchor),
             titleLabel.topAnchor.constraint(equalTo: imageView.layoutMarginsGuide.topAnchor),
-            titleLabel.heightAnchor.constraint(greaterThanOrEqualToConstant: String.calculateLabelHeight(targetString: "슬기로운\n자연생활", fontSize: 34, weight: .init(rawValue: 500))),
             titleLabel.heightAnchor.constraint(lessThanOrEqualTo: imageView.heightAnchor, multiplier: 1/3),
             titleLabel.widthAnchor.constraint(equalTo: imageView.widthAnchor, multiplier: 1/2),
 
             descriptionLabel.leadingAnchor.constraint(equalTo: titleLabel.leadingAnchor),
             descriptionLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 16),
             descriptionLabel.widthAnchor.constraint(equalTo: imageView.widthAnchor, multiplier: 1/2),
-            descriptionLabel.heightAnchor.constraint(greaterThanOrEqualToConstant: String.calculateLabelHeight(targetString: "에어비앤비가 엄선한\n위시리스트를 만나보세요.", fontSize: 17, weight: .init(rawValue: 400))),
             descriptionLabel.heightAnchor.constraint(lessThanOrEqualTo: titleLabel.heightAnchor),
 
             heroButton.leadingAnchor.constraint(equalTo: titleLabel.leadingAnchor),
             heroButton.topAnchor.constraint(equalTo: descriptionLabel.bottomAnchor, constant: 16),
-            heroButton.layoutMarginsGuide.heightAnchor.constraint(equalToConstant: String.calculateLabelHeight(targetString: "여행아이디어", fontSize: 17, weight: .init(rawValue: 400))),
             heroButton.widthAnchor.constraint(greaterThanOrEqualToConstant: 165),
             heroButton.widthAnchor.constraint(lessThanOrEqualTo: imageView.widthAnchor)
         ])
