55d8840bbd719800cce79516018fcfdcab42ca39
PhilSoGooood <peelhw@gmail.com>
2022-06-08 16:40:45 +0900

    fix: 라이브러리를 통한 시간 및 거리 단위 변환
    - 내장 라이브러리를 통해 시간 단위를 초에서 분으로 변경
    - 외부 라이브러리를 통해 거리 단위를 미터에서 킬로미터로 변경

--- BE/src/main/java/com/team11/airbnb/openapi/DistrictSearchService.java
+++ BE/src/main/java/com/team11/airbnb/openapi/DistrictSearchService.java
@@ -1,9 +1,11 @@
 package com.team11.airbnb.openapi;
 
+import java.time.Duration;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
@@ -13,11 +15,15 @@ import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import com.digidemic.unitof.UnitOf;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import lombok.extern.slf4j.Slf4j;
+
 @Service
+@Slf4j
 public class DistrictSearchService {
 
     private String kakaokey = "a1d0ecbd325633346f2938e35d6db4d4";
@@ -27,18 +33,18 @@ public class DistrictSearchService {
         District[] districts = District.values();
         List<DistanceInfoResponse> distanceInfoResponses = new ArrayList<>();
         for (District district : districts) {
-            DistanceInfo distanceInfo = calculateDistanceAndTime(position, district.getLongtitude(),
+            Map<String, String> result = calculateDistanceAndTime(position, district.getLongtitude(),
                 district.getLatitude());
-            String distance = String.valueOf(distanceInfo.getDistanceByKiloMeters());
-            String duration = String.valueOf(distanceInfo.getTimeByMinutes());
+            String distance = result.get("distance");
+            String time = result.get("time");
             distanceInfoResponses.add(
-                new DistanceInfoResponse(district.name(), distance, duration));
+                new DistanceInfoResponse(district.name(), distance, time));
         }
 
         return distanceInfoResponses;
     }
 
-    private DistanceInfo calculateDistanceAndTime(Position position, double destinationX,
+    private Map<String, String> calculateDistanceAndTime(Position position, double destinationX,
         double destinationY) throws
         JsonProcessingException {
         RestTemplate restTemplate = new RestTemplate();
@@ -47,19 +53,32 @@ public class DistrictSearchService {
             new HttpEntity<>(getHeaders()), String.class);
 
         ObjectMapper objectMapper = new ObjectMapper();
+        Map<String, String> result = new HashMap<>();
 
         JsonNode body = objectMapper.readTree(response.getBody());
         JsonNode routes = body.get("routes");
         JsonNode route = routes.get(0);
         int resultCode = route.get("result_code").asInt();
+        log.info("result code = {}", resultCode);
         if (resultCode != 0) {
-            return new DistanceInfo(-999, -999);
+            result.put("time", "-999");
+            result.put("distance", "-999");
+            return result;
         }
         JsonNode summary = route.get("summary");
         double distanceByMeters = summary.get("distance").asDouble();
-        double timeBySeconds = summary.get("duration").asDouble();
+        Long timeBySeconds = summary.get("duration").asLong();
+        log.warn("distance = {}, time  = {}", distanceByMeters, timeBySeconds);
 
-        return new DistanceInfo(distanceByMeters, timeBySeconds);
+
+        Duration duration = Duration.ofSeconds(timeBySeconds);
+        String timeByMinutes = String.valueOf(duration.toMinutes());
+        UnitOf.Length distanceUnitConverter = new UnitOf.Length().fromMeters(distanceByMeters);
+        String distanceByKilometers = String.valueOf(distanceUnitConverter.toKilometers());
+        result.put("time", timeByMinutes);
+        result.put("distance", distanceByKilometers);
+
+        return result;
     }
 
     private MultiValueMap<String, String> getHeaders() {
@@ -75,6 +94,7 @@ public class DistrictSearchService {
         String url = "https://apis-navi.kakaomobility.com/v1/directions";
         String origin = originX + "," + originY;
         String destination = destinationX + "," + destinationY;
+        log.info("origin = {}, destination = {}", origin, destination);
         return UriComponentsBuilder.fromHttpUrl(url)
             .queryParam("origin", origin)
             .queryParam("destination", destination)
